d0cbef458f1eb9d2faa7e94145c019026959a6a5
Himanshu Bhanpurkar <hbhanpurkar@gmail.com>
2020-07-09 01:00:21 +0530

    New function list added

---
+++ RDIP/Codes/Libraries/app.js
@@ -0,0 +1,17 @@
+function Introduction(){
+    document.getElementById('result').innerHTML = 'A sentence can become more complex, if more than one verb is present or by joining two sentences or words using conjunctions or by some other methods.'
+}
+function Theory(){
+    document.getElementById('result').innerHTML = 'Theory'
+        
+}
+function Objective(){
+    document.getElementById('result').innerHTML = 'The objective of this experiment is to know how to form logically correct sentences from the given words'
+    
+}
+function Experiment(){
+    document.getElementById('option1').innerHTML = 'English'
+    document.getElementById('option2').innerHTML = 'Hindi'
+       
+        
+}
